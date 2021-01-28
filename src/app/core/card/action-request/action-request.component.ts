b5d4b752d190b93e1e6a86c6bcbd1756b99cad8e
StefanoChessa <sr.chessa@tiscali.it>
2021-01-28 18:33:41 +0100

    aggiunta player video action request

--- src/app/core/card/action-request/action-request.component.ts
+++ src/app/core/card/action-request/action-request.component.ts
@@ -1,8 +1,12 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {DialogModel} from '../../../shared/_models/dialog.model';
-import {Info} from "../../../shared/_models/info.model";
-import {DataService} from "../../../shared/_services/data.service";
+import {Info} from '../../../shared/_models/info.model';
+import {DataService} from '../../../shared/_services/data.service';
+import {SafespotterService} from '../../../shared/_services/safespotter.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {LampStatus} from '../../../shared/_models/LampStatus';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-action-request',
@@ -11,20 +15,37 @@ import {DataService} from "../../../shared/_services/data.service";
 })
 export class ActionRequestComponent implements OnInit {
   title: string;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogModel,
               private datasev: DataService,
-  ) {}
+              private safespotter: SafespotterService
+  ) {
+  }
+  formatUrl = environment.protocol + environment.host + ':' + environment.port;
+  @Input() isVideoURLReady;
+  videoURL;
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.getVideoURL();
   }
 
+  getVideoURL() {
+    let videoURL = '';
+    this.safespotter.getLampStatus(this.data.id).subscribe(
+      data => {
+        videoURL = String(Object.entries((data['data'])[1])[5][1]);
+        this.videoURL = this.formatUrl + videoURL;
+        this.isVideoURLReady = true;
+      }
+    );
+  }
 
   actionRequest(info: Info) {
     const param = {
-      id : info.id,
-      street : info.street,
-      condition : info.condition,
-      critical_issues : 0
+      id: info.id,
+      street: info.street,
+      condition: info.condition,
+      critical_issues: 0
     };
 
     this.datasev.actionRequest(param).subscribe();
