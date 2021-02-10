baba72508521732bfb426dd2887357a5b0267c9f
StefanoChessa <sr.chessa@tiscali.it>
2021-02-10 10:41:16 +0100

    in progress: modifica modal per visualizzazione telecamere

--- src/app/core/card/video/video.component.ts
+++ src/app/core/card/video/video.component.ts
@@ -1,10 +1,11 @@
 import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {MatVideoComponent} from 'mat-video/lib/video.component';
 import JSMpeg from 'jsmpeg-player';
-import { ONVIFService } from 'onvif-rx-angular';
+import {ONVIFService} from 'onvif-rx-angular';
 import {DataService} from '../../../shared/_services/data.service';
 import {MatSelect} from '@angular/material/select';
 import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
+
 
 @Component({
   selector: 'app-video',
@@ -20,9 +21,9 @@ export class VideoComponent implements OnInit, AfterViewInit {
   // video: HTMLVideoElement;
   modalRef: BsModalRef;
   title: string;
-  selected = '';
-  defaultSelected: string;
+  selected: string;
   safeList: any;
+
   constructor(
     private renderer: Renderer2,
     private datasev: DataService,
@@ -33,27 +34,30 @@ export class VideoComponent implements OnInit, AfterViewInit {
 
   async ngOnInit() {
     this.safeList = await this.datasev.getData().toPromise();
-    this.defaultSelected = this.safeList[0].id.toString();
+    this.selected = this.safeList[0].id.toString(); // il primo nella lista
   }
 
   ngAfterViewInit() {
     this.matSelect.valueChange.subscribe(value => {
       this.selected = value;
-      console.log("value selected ", value);
-    //   const player = new JSMpeg.Player ('ws://localhost:9999', {canvas: document.getElementById('canvas'), autoplay: true, audio: false, loop: true});
+      //console.log('value selected ', value);
+      //   const player = new JSMpeg.Player ('ws://localhost:9999', {canvas: document.getElementById('canvas'), autoplay: true, audio: false, loop: true});
     });
   }
 
   openModal(modal) {
     this.modalRef = this.modalService.show(modal,
       {
-        class: 'modal-md modal-dialog-centered',
+        class: 'modal-lg modal-dialog-centered',
         keyboard: false
       });
   }
 
   decline = (): void => {
     this.modalRef.hide();
-  };
+  }
 
+  getIndexFromId(id) {
+    return this.safeList.findIndex(obj => obj.id === parseInt(id,10));
+  }
 }
