1a069c93c6f3f53b0615c3c923afdcc55cbddc90
StefanoChessa <sr.chessa@tiscali.it>
2021-02-15 16:08:01 +0100

    in progress: componente configuratore lampione

--- src/app/core/card/lamppost-configuration/lamppost-configuration.component.ts
+++ src/app/core/card/lamppost-configuration/lamppost-configuration.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {DialogModel} from '../../../shared/_models/dialog.model';
+import {MatTableDataSource} from '@angular/material';
+import {LampStatus} from '../../../shared/_models/LampStatus';
 
 @Component({
   selector: 'app-lamppost-configuration',
@@ -9,13 +11,17 @@ import {DialogModel} from '../../../shared/_models/dialog.model';
 })
 export class LamppostConfigurationComponent implements OnInit {
   title: string;
+  displayedColumns = ['Anomalia', 'Allerta verde', 'Allerta gialla', 'Allerta arancione', 'Allerta rossa'];
+  dataSource = new MatTableDataSource();
+  criticalIssues = [{name: 'Marcia contromano'}, {name: 'Pedone sulla carreggiata'}, {name: 'Traffico congestionato'}];
+
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogModel
   ) {
   }
 
   async ngOnInit() {
-
+    this.dataSource = new MatTableDataSource(this.criticalIssues);
   }
 
 }
