4f65c5a650f45add9c90245c9418ae261a116dea
StefanoChessa <sr.chessa@tiscali.it>
2021-06-09 10:22:59 +0200

    fix configurazione lampione per versioni mobili

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const host = 'localhost';
+const host = '192.168.1.5';
 const port = '8080';
 const protocol = 'http://';
 export const environment = {
