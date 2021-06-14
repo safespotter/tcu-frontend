894546d65b869e91d266e806b9a1c1601347f1e5
StefanoChessa <sr.chessa@tiscali.it>
2021-06-14 11:13:16 +0200

    in progress: aggiunta markers multipli in mappa

--- src/app/core/card/map/map.component.ts
+++ src/app/core/card/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+import {Component, OnInit} from '@angular/core';
+import {AgmCoreModule} from '@agm/core';
 
 @Component({
   selector: 'app-map',
@@ -8,10 +8,22 @@ import { AgmCoreModule } from '@agm/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-  zoom = 17
-  center: google.maps.LatLngLiteral
+
+  // lat = 39.25202;
+  // long = 9.13802;
+  zoom = 17;
+
+  labelOptions: {
+    color: 'black',
+    fontFamily: '',
+    fontSize: '14px',
+    fontWeight: 'bold',
+  };
+
+  center: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
     mapTypeId: 'roadmap',
     zoomControl: false,
@@ -19,50 +31,70 @@ export class MapComponent implements OnInit {
     disableDoubleClickZoom: true,
     maxZoom: 30,
     minZoom: 8,
-  }
+  };
+  markers = [
+    {
+      lat: 39.25202,
+      lng: 9.13802,
+      label: 'Via Cesare Cabras'
+    },
+    {
+      lat: 39.25791,
+      lng: 9.144299,
+      label: 'Via Seneca'
+    }
+  ];
+  // zoom = 17
 
+
+  //
   // ngOnInit() {
   //
   //   navigator.geolocation.getCurrentPosition(position => {
   //     this.center = {
   //       lat: 39.252341,
   //       lng: 9.137819
-  //     }
-  //   })
+  //     };
+  //   });
   //
   // }
-  lat : number;
-  lng : number;
-  labelOptions = {
-    color: 'black',
-    fontFamily: '',
-    fontSize: '14px',
-    fontWeight: 'bold',
-    // text: "SMARTLAMP VIA CABRAS"
-  }
 
+  // lat : number;
+  // lng : number;
+  // labelOptions = {
+  //   color: 'black',
+  //   fontFamily: '',
+  //   fontSize: '14px',
+  //   fontWeight: 'bold',
+  //   // text: "SMARTLAMP VIA CABRAS"
+  // }
+  //
   ngOnInit() {
-    this.prepareMap() ;
+    // this.prepareMap() ;
   }
 
-  prepareMap() {
-    this.lat = 39.251896;
-    this.lng = 9.138610;
-    // this.lat = 39.487516;
-    // this.lng = 9.064641;
-  }
+  // prepareMap() {
+  //   this.lat = 39.251896;
+  //   this.long = 9.138610;
+  //   // this.lat = 39.487516;
+  //   // this.lng = 9.064641;
+  //   // this.markers;
+  // }
 
   zoomIn() {
     if (this.zoom < this.options.maxZoom) this.zoom++;
   }
 
+  //
   zoomOut() {
     if (this.zoom > this.options.minZoom) this.zoom--;
   }
 
+  //
   onMapReady($event) {
     const trafficLayer = new google.maps.TrafficLayer();
     trafficLayer.setMap($event);
   }
+
 }
 
