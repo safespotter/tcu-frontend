45276e0d61ecc552b4cdedc3b3068446d452e45a
StefanoChessa <sr.chessa@tiscali.it>
2021-06-16 16:50:18 +0200

    passaggio parametri per nuovo componente cam

--- src/app/core/card/cam/cam.component.ts
+++ src/app/core/card/cam/cam.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
+import {DataService} from '../../../shared/_services/data.service';
+import JSMpeg from 'jsmpeg-player';
 
 @Component({
   selector: 'app-cam',
@@ -8,12 +10,34 @@ import {Router} from '@angular/router';
 })
 export class CamComponent implements OnInit {
 
-  constructor(private router: Router) {
-    console.log(this.router.getCurrentNavigation().extractedUrl.queryParams.cam);
+  // @ViewChild('streaming', {static: false}) streamingcanvas: ElementRef;
+  @Input() isLampDataReady;
+  lamp_id;
+  lamp_data;
+  constructor(private router: Router,
+              private datasev: DataService) {
+    this.lamp_id = this.router.getCurrentNavigation().extractedUrl.queryParams.cam;
   }
 
   ngOnInit(): void {
+    this.getLampData(this.lamp_id);
 
+    // const player = new JSMpeg.Player('rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov', {
+    //   canvas: this.streamingcanvas, autoplay: true, audio: false, loop: true
+    // });
+  }
+
+  getLampData(id) {
+    const data = this.datasev.getData().subscribe(
+      result => {
+        for(const el of Object.values(result)){
+          if (el.id == this.lamp_id){
+            this.isLampDataReady = true;
+            this.lamp_data = el;
+          }
+        }
+      }
+    );
   }
 
 }
