30d966b6f66929588e63522a139a1762ddc06642
StefanoChessa <sr.chessa@tiscali.it>
2021-07-06 15:37:13 +0200

    traduzione meteo

--- src/app/core/card/weather/weather.component.ts
+++ src/app/core/card/weather/weather.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { WeatherModel } from '../../../shared/_models/weather.model';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {WeatherModel} from '../../../shared/_models/weather.model';
 import {WeatherService} from '../../../shared/_services/weather.service';
 
 @Component({
@@ -13,7 +13,8 @@ export class WeatherComponent implements OnInit {
   constructor(
     private httpClient: HttpClient,
     private weatherService: WeatherService
-  ) { }
+  ) {
+  }
 
   currentWeather: WeatherModel;
   dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
@@ -25,6 +26,30 @@ export class WeatherComponent implements OnInit {
 
   getWeather(): void {
     this.weatherService.getLive().subscribe(data => this.currentWeather = data);
+  }
+
+  conditionConverter(condition) {
+    switch (condition) {
+      case 'clear':
+        return 'Sereno';
+      case 'thunders':
+        return 'Temporale';
+      case 'drizzle':
+      case 'light rain':
+        return 'Pioggia leggera';
+      case 'rain':
+        return 'Pioggia';
+      case 'snow':
+        return 'Neve';
+      case 'fog':
+        return 'Nebbia';
+      case 'light clouds':
+        return 'parzialmente nuvoloso';
+      case 'clouds':
+        return 'Nuvoloso';
+      default:
+        return 'errore';
+    }
   }
 
 }
